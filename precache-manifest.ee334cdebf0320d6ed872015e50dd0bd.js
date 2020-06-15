712608239a41d7fb7cbadee3695d3064c5085f52
Mayank Bhagya <mayankbhagya@gmail.com>
2020-06-16 03:03:26 +0530

    Updates

---
+++ precache-manifest.ee334cdebf0320d6ed872015e50dd0bd.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd841849beff1f5c78ace9027d9ac7e0",
+    "url": "/covid19india-react/index.html"
+  },
+  {
+    "revision": "d129234e99d678f083fe",
+    "url": "/covid19india-react/static/css/2.ab33d419.chunk.css"
+  },
+  {
+    "revision": "5a9fd072b25e63175349",
+    "url": "/covid19india-react/static/css/App.264e1a4c.chunk.css"
+  },
+  {
+    "revision": "0cc222b09c4a333a3205",
+    "url": "/covid19india-react/static/js/0.14dca4c7.chunk.js"
+  },
+  {
+    "revision": "72b99e373e7bdaddba36",
+    "url": "/covid19india-react/static/js/1.ade735cc.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/covid19india-react/static/js/1.ade735cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d129234e99d678f083fe",
+    "url": "/covid19india-react/static/js/2.2f3a5268.chunk.js"
+  },
+  {
+    "revision": "9db75caed30a1828fb36",
+    "url": "/covid19india-react/static/js/26.849d976c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid19india-react/static/js/26.849d976c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a1e2bb55dc0f679456b",
+    "url": "/covid19india-react/static/js/27.6b06a61f.chunk.js"
+  },
+  {
+    "revision": "d14d0ad472def4bd8256cc358bc09841",
+    "url": "/covid19india-react/static/js/27.6b06a61f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d7b5cbc689dea4f433f",
+    "url": "/covid19india-react/static/js/28.77dca613.chunk.js"
+  },
+  {
+    "revision": "77530437525761b8a452",
+    "url": "/covid19india-react/static/js/29.34b9959b.chunk.js"
+  },
+  {
+    "revision": "6a54415d7762c984f6576b7ba16116ad",
+    "url": "/covid19india-react/static/js/29.34b9959b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a48eda79d9ed774b06cb",
+    "url": "/covid19india-react/static/js/3.ce488639.chunk.js"
+  },
+  {
+    "revision": "50bcdbf2c39fe2747058",
+    "url": "/covid19india-react/static/js/30.c4be960c.chunk.js"
+  },
+  {
+    "revision": "717e242bf7c42853e29c",
+    "url": "/covid19india-react/static/js/31.8b90aae9.chunk.js"
+  },
+  {
+    "revision": "5000e9c9c45d947fe942",
+    "url": "/covid19india-react/static/js/32.78724339.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/covid19india-react/static/js/32.78724339.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bb9444633b8f54137c2",
+    "url": "/covid19india-react/static/js/33.ef4b826b.chunk.js"
+  },
+  {
+    "revision": "771ca053e548612f67b3",
+    "url": "/covid19india-react/static/js/4.c316c140.chunk.js"
+  },
+  {
+    "revision": "3c048b2169e2783b13e7",
+    "url": "/covid19india-react/static/js/5.0ac7e880.chunk.js"
+  },
+  {
+    "revision": "fed04c33aef480da5f67",
+    "url": "/covid19india-react/static/js/About.d45acfb9.chunk.js"
+  },
+  {
+    "revision": "640e560289e5e44aacf0",
+    "url": "/covid19india-react/static/js/Actions.eed4b962.chunk.js"
+  },
+  {
+    "revision": "5a9fd072b25e63175349",
+    "url": "/covid19india-react/static/js/App.748934e7.chunk.js"
+  },
+  {
+    "revision": "474db44e49827cd421f5",
+    "url": "/covid19india-react/static/js/Demographics.bdea066c.chunk.js"
+  },
+  {
+    "revision": "a1594d6e6ea48acbb43f",
+    "url": "/covid19india-react/static/js/Essentials.68a7e9f4.chunk.js"
+  },
+  {
+    "revision": "765862605b5063cd6618",
+    "url": "/covid19india-react/static/js/Footer.42eb1a37.chunk.js"
+  },
+  {
+    "revision": "b19fdeafd7063c5d8e06",
+    "url": "/covid19india-react/static/js/Home.07316c89.chunk.js"
+  },
+  {
+    "revision": "edc0e03f7b0b2006ea3c",
+    "url": "/covid19india-react/static/js/LanguageSwitcher.b8894040.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/covid19india-react/static/js/LanguageSwitcher.b8894040.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d32ea52862f803a2871f",
+    "url": "/covid19india-react/static/js/Level.5dee6409.chunk.js"
+  },
+  {
+    "revision": "041234ea3ca6c6d5d13f",
+    "url": "/covid19india-react/static/js/MapExplorer.d32d6374.chunk.js"
+  },
+  {
+    "revision": "53bb8c13367e2f0871ff",
+    "url": "/covid19india-react/static/js/MapVisualizer.a2cde746.chunk.js"
+  },
+  {
+    "revision": "5c8f6151af5e5dbd2ca4",
+    "url": "/covid19india-react/static/js/Minigraph.191e084e.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/covid19india-react/static/js/Minigraph.191e084e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "019f077aa2b9a9074960",
+    "url": "/covid19india-react/static/js/Row.5083614f.chunk.js"
+  },
+  {
+    "revision": "0b117057a87d6b640f49",
+    "url": "/covid19india-react/static/js/State.efc6e026.chunk.js"
+  },
+  {
+    "revision": "3127a9621b818270440e",
+    "url": "/covid19india-react/static/js/Table.1db749ec.chunk.js"
+  },
+  {
+    "revision": "528fdae7b524e558be2c",
+    "url": "/covid19india-react/static/js/TimeSeries.45cf71aa.chunk.js"
+  },
+  {
+    "revision": "f90936a8510a6c9304ea",
+    "url": "/covid19india-react/static/js/TimeSeriesExplorer.5f125aaf.chunk.js"
+  },
+  {
+    "revision": "b104179b62400e38a9b0",
+    "url": "/covid19india-react/static/js/Updates.8ca24909.chunk.js"
+  },
+  {
+    "revision": "a85c34f589feed2a60c2",
+    "url": "/covid19india-react/static/js/main.62d2dac9.chunk.js"
+  },
+  {
+    "revision": "e17c10928f6e74802a03",
+    "url": "/covid19india-react/static/js/runtime-main.08dd142d.js"
+  },
+  {
+    "revision": "013f0b90a4cae7b8bbf13e3fd9e7dc4a",
+    "url": "/covid19india-react/static/media/getFetch.013f0b90.cjs"
+  }
+]);
